70af5d191626861968e41cb3857179a129bd9330
Dujol05 <dujol2258@gmail.com>
2024-04-13 18:02:38 +0900

    (2024.04.13) 첫 커밋

---
+++ Mamory/Memory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memory
+{
+    internal class Memory
+    {
+
+        class Memory
+
+        {  
+            public int value = 10;
+            public string name = "홍길동";
+            public int attack = 100;
+            public float attackRange = 3.14f;
+
+
+            public void AddValue(int _value)
+            {
+                value = _value + this.value;
+            }
+        }
+    }
+}
---
+++ Mamory/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Memory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = 10;
+
+            value = SomeFunction(value);
+
+            Console.ReadLine();
+            Console.WriteLine(value);
+
+            Memory myMemory = new Memory();
+            myMemory.SomeFunction(value);
+            Console.WriteLine("myMemory의 value: " + myMemory.value);
+        }
+
+
+        static void SomeFunction(int value)
+        {
+            value = 100;
+        }
+
+        static void OtherFunction(int value, Memory _memory)
+        {
+
+        }
+    }
+}
